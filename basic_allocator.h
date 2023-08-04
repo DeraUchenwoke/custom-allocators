421f59bca3e3242b31c1cbd45797330e66c2bf98
Dera Uchenwoke <uchidera1@gmail.com>
2023-08-04 20:31:16 +0100

    added basic allocator class functions and generic typename

--- basic_allocator.h
+++ basic_allocator.h
@@ -1,19 +1,21 @@
 #pragma once
 
-template <typename T> class Allocator
+#include <cstddef> // for 'size_t'
+
+template <typename T> 
+class Allocator
 {
-public: 
-	
-	// Constructor 
-	Allocator() = default; // default = create objects of this class without an initialiser pg 438
+	using Type = typename T::value_type; // user-defined value
 
-	[[nodiscard]] constexpr T* allocate(size_t n) 
-	{ // allocate memory for n objects of type T
-		return new (n * sizeof(T)); // pointer to the first of n objects allocated in memory but have not been constructed yet
-	}
-
-	void deallocate(T* ptr, size_t n) noexcept 
-	{
-		return delete(ptr, n * sizeof(T));
+	[[nodiscard]] Type* allocate(size_t num) return new Type[num]; // initialises the memory requested
+	void deallocate(Type* p, size_t num) noexcept { // deallocate should not throw runtime exceptions
+		delete(p); 
 	}
 };
+
+/*
+* 'value_type' meaning: https://cplusplus.com/forum/beginner/275487/
+* 'using' logic/meaning: https://stackoverflow.com/questions/20790932/what-is-the-logic-behind-the-using-keyword-in-c
+* New operator vs. operator new: https://www.geeksforgeeks.org/new-vs-operator-new-in-cpp/
+* 
+*/
